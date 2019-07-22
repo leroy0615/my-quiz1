592b02efcf125d2a8fda6fe7335f0cffd739e1a9
leroy0615 <leroy0615@gmail.com>
2019-07-22 15:25:53 +0000

    part 2 of quiz1

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,21 @@
 class HomesController < ApplicationController
 	def index
+		@homes=Home.all
+
+	end
+
+	def new
+		@home = Home.new
+	end
+
+	def create
+		Home.create(home_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def home_params
+		params.require(:home).permit(:name, :description, :grade)
 	end
 end
